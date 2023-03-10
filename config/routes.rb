5e05e8c20bedb3a7ea29e4914445fe7cbdda4e97
Philipp Staehr <philippstaehr@gmail.com>
2023-03-10 11:42:37 +0100

    Added all movies index view

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # As a user, I can see all my movie lists
+  root to: 'lists#index'
+
+  resources :lists, only: [:index]
+  # As a user, I can create a movie list
+  # As a user, I can see the details of a movie list
 end
