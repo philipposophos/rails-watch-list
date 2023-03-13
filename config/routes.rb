1e04c0c3799968a89a9003a73b549f87012704ab
Philipp Staehr <philippstaehr@gmail.com>
2023-03-13 08:39:39 +0100

    watch list Zwischenstand before bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
   end
+
+  resources :bookmarks, only: [:destroy]
 end
